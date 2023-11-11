6f984fea810a32b15183e9f7f743f52e4b26c55a
Paresh-Kumar14 <pareshkbehera14@gmail.com>
2023-11-11 21:52:32 +0530

    Added Vercel.json file and Updated Live URL

--- Backend/index.js
+++ Backend/index.js
@@ -6,26 +6,26 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-      origin: ["http://localhost:3000", "https://skynet-chat.vercel.app"],
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      credentials: true,
-      headers: ["Authorization", "Content-Type"],
+        origin: ["http://localhost:5173", "https://skynet-chat.vercel.app"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+        headers: ["Authorization", "Content-Type"],
     })
-  );
+);
 
-app.post("/authenticate",async(req,res)=>{
-    const { username }= req.body;
-    try{
+app.post("/authenticate", async (req, res) => {
+    const { username } = req.body;
+    try {
         const p = await axios.put(
             'https://api.chatengine.io/users/',
-            {username:username,secret:username,first_name:username},{headers:{"private-key":'e256dc65-8062-4da3-8118-79075ea2ecee'}}
+            { username: username, secret: username, first_name: username }, { headers: { "private-key": 'e256dc65-8062-4da3-8118-79075ea2ecee' } }
         )
         return res.status(p.status).json(p.data)
-    } catch (e){
+    } catch (e) {
         return res.status(e.response.status).json(e.response.data)
     }
-    
-    
+
+
 });
 
 app.listen(3484);
